a21f85cdba55153013acd3ac24a26a89c2eb8d75
faithOta1 <faith_ota1@baylor.edu>
2024-10-31 19:15:03 -0500

    Add files via upload

---
+++ RBT.h
@@ -0,0 +1,47 @@
+/*
+ *Author: Faith Ota
+ *Title: Red-Black Tree Implementation
+ *Date Last Modified: 10/31/2024
+ */
+#ifndef RBT_h
+#define RBT_h
+
+using namespace std;
+
+template <typename T>
+struct Node{
+    T data;
+    Node<T>* left;
+    Node<T>* right;
+    Node<T>* parent;
+    int height=0;
+    bool isRed=true;
+    
+    Node(const T& d, Node<T>* l, Node<T>* r, Node<T>* p): data(d), left(l),
+        right(r), parent(p){}
+};
+
+template <typename T>
+class RBT{
+private:
+    Node<T>* root;
+    //single rotates
+    void rotateL(){}
+    void rotateR(){}
+    
+    //rebalance colors
+    void rebalance(){}
+    
+    //insert/remove with a pointer
+    void insert(T& val, Node* p){}
+    void remove(T& val, Node* p){}
+    
+    
+public:
+    RBT():root(nullptr){}
+    ~RBT(){ root = nullptr; }
+    
+    T& find(
+};
+
+#endif /* RBT_h */
