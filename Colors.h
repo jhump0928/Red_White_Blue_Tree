a0947c867e66a0ecda7ff75b663027773d95d4e8
Mac Johnson <152658879+macj2005@users.noreply.github.com>
2024-12-06 13:30:03 -0600

    Create Colors.h

---
+++ Colors.h
@@ -0,0 +1,22 @@
+//
+//  Colors.h
+//  3334 Group Project
+//
+//  Created by Mac J on 12/3/24.
+//
+
+#ifndef Colors_h
+#define Colors_h
+
+#include "SDL_Plotter.h"
+
+using namespace std;
+
+color lightBlue(173, 216, 230);
+color darkBlue(11, 11, 69);
+color red(130, 0, 0);
+color black(0, 0, 0);
+color yellow(207, 255, 4);
+
+
+#endif /* Colors_h */
