40d173a3bdddbd000c0eed09d7d1fd08592eb351
Jonathan Humphries <165952594+jhump0928@users.noreply.github.com>
2024-12-07 15:43:58 -0600

    Add files via upload

---
+++ colors.h
@@ -0,0 +1,18 @@
+//
+// Created by jhump on 11/13/2024.
+//
+
+#ifndef RBT_GROUP_PROJECT_COLORS_H
+#define RBT_GROUP_PROJECT_COLORS_H
+
+#include "SDL_Plotter.h"
+
+using namespace std;
+
+color lightBlue(173,216,230);
+color darkBlue(11,11,69);
+color red(166,44,43);
+color yellow(255,244,79);
+color black(12,11,10);
+
+#endif //RBT_GROUP_PROJECT_COLORS_H
